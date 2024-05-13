4b5b30d6bd3a7d145d5373cd35bd7b0b95e37f31
Jonas A. Ribeiro <99909784+jonasaribeiro@users.noreply.github.com>
2024-05-13 12:29:19 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+# Esperar até que o PostgreSQL esteja pronto
 while ! pg_isready -h db -p 5432 -U onguser; do
     echo "Aguardando o DB..."
     sleep 2
 done
 
-npx primsa migrate deploy
+echo "PostgreSQL pronto. Executando migrações do Prisma..."
+npx prisma migrate deploy
 
+echo "Migrações aplicadas. Iniciando a aplicação..."
 node start
