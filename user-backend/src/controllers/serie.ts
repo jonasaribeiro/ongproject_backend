3ba23b759bc52da89943e1cf9865265ee014b6a7
gpmlad <giovanniperotto@hotmail.com>
2024-07-22 22:36:22 -0300

    feat: add series routes

--- user-backend/src/controllers/serie.ts
+++ user-backend/src/controllers/serie.ts
@@ -3,7 +3,12 @@ import { SerieService } from "../services/serie";
 
 class SerieController {
   static getAll = async (req: Request, res: Response) => {
-    const series = await SerieService.getAll();
+    const { category, age } = req.query;
+
+    const categoryId = typeof category === "string" ? category : undefined;
+    const ageRatingId = typeof age === "string" ? age : undefined;
+
+    const series = await SerieService.getAll(categoryId, ageRatingId);
 
     res.status(200).json(series);
   };
--- user-backend/src/services/serie.ts
+++ user-backend/src/services/serie.ts
@@ -2,9 +2,58 @@ import prisma from "../config/prisma";
 import { SSerieResponse, TSerieResponse } from "../schemas";
 
 class SerieService {
-  static getAll = async (): Promise<TSerieResponse[]> => {
-    const series = await prisma.serie.findMany();
-    return series;
+  static getAll = async (
+    categoryId?: string,
+    ageId?: string
+  ): Promise<TSerieResponse[]> => {
+    if (categoryId && ageId) {
+      const series = await prisma.movie.findMany({
+        where: {
+          AND: [
+            {
+              categories: {
+                some: {
+                  categoryId: categoryId,
+                },
+              },
+            },
+            {
+              ageRatings: {
+                some: {
+                  ageRatingId: ageId,
+                },
+              },
+            },
+          ],
+        },
+      });
+      return SSerieResponse.array().parse(series);
+    } else if (ageId) {
+      const series = await prisma.movie.findMany({
+        where: {
+          ageRatings: {
+            some: {
+              ageRatingId: ageId,
+            },
+          },
+        },
+      });
+      return SSerieResponse.array().parse(series);
+    } else if (categoryId) {
+      const series = await prisma.serie.findMany({
+        where: {
+          categories: {
+            some: {
+              categoryId: categoryId,
+            },
+          },
+        },
+      });
+      return SSerieResponse.array().parse(series);
+    } else {
+      const series = await prisma.movie.findMany();
+      return SSerieResponse.array().parse(series);
+    }
   };
 
   static getById = async (id: string): Promise<TSerieResponse> => {
