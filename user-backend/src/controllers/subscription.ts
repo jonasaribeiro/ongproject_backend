1050145714a5255a47c423b83646941c10509f13
Jonas <jonasafribeiro@gmail.com>
2024-09-17 15:22:13 -0300

    Adicionado comentário de TODO especificando  billingPortal

--- user-backend/src/controllers/subscription.ts
+++ user-backend/src/controllers/subscription.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from "express";
 
 class SubscriptionController {
-  static generateCheckoutLink = async (req: Request, res: Response) => {};
+  static generateCheckoutLink = async (req: Request, res: Response) => {
+    //
+  };
 
-  static generateClientLink = async (req: Request, res: Response) => {};
+  static generateClientLink = async (req: Request, res: Response) => {
+    // stripe.billingPortal.sessions.create
+  };
 }
 
 export default SubscriptionController;
