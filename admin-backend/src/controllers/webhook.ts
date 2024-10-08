2eebdfda9f1a594962fbe469c3aa551ab3b0c08b
Jonas <jonasafribeiro@gmail.com>
2024-10-08 20:22:11 -0300

    Feat: Adicionado handler para webhook

--- admin-backend/src/controllers/webhook.ts
+++ admin-backend/src/controllers/webhook.ts
@@ -4,20 +4,6 @@ class WebhookController {
   static handle = async (req: Request, res: Response) => {
     const body = req.body;
 
-    // invoice.paid
-    //
-
-    // Pagamento concluido com sucesso
-    // status tem que estar "active"
-
-    // Assinatura deletada
-    // status diferente de "active"
-
-    // Adicionar uma tabela para manipular informações sobre assinatura
-    // Subscription
-    // Status
-    // ActiveTill
-
     res.send();
   };
 }
--- user-backend/src/controllers/subscription.ts
+++ user-backend/src/controllers/subscription.ts
@@ -3,7 +3,19 @@ import stripe from "../config/stripe";
 
 class SubscriptionController {
   static generateCheckoutLink = async (req: Request, res: Response) => {
-    //
+    const { successUrl, stripeId, priceId } = req.body;
+
+    const session = await stripe.checkout.sessions.create({
+      success_url: successUrl,
+      line_items: [
+        {
+          price: priceId,
+        },
+      ],
+      mode: "subscription",
+    });
+
+    res.json({ url: session.url });
   };
 
   static generateClientLink = async (req: Request, res: Response) => {
